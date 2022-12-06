13945fd6225193a63cae561fd8d405a90fd40c1d
Rapilias <91034223+Rapilias@users.noreply.github.com>
2022-12-07 06:48:40 +0900

    Revert "ファイル分割出力対応/AnimatorCodeGenConfigureの設定値が別アセットに複製される問題修正"

--- UnityTypedAnimator/Assets/UnityTypedAnimator/Editor/TriggerParameterWriter.cs
+++ UnityTypedAnimator/Assets/UnityTypedAnimator/Editor/TriggerParameterWriter.cs
@@ -5,17 +5,17 @@ namespace EgoParadise.UnityTypedAnimator.Editor
 {
     public class TriggerParameterWriter : IParameterWriter
     {
-        public void WriteGetParameterFunction(StringBuilder builder, string name, int baseIndent)
+        public void WriteGetParameterFunction(StringBuilder builder, string name, int baseIndent, int indentStep)
         {
         }
 
-        public void WriteSetParameterFunction(StringBuilder builder, string name, int baseIndent)
+        public void WriteSetParameterFunction(StringBuilder builder, string name, int baseIndent, int indentStep)
         {
             var indent = new string(' ', baseIndent);
             builder.Append($"{indent}public void Set{name}() => this.animator.SetTrigger(this.{name}Id);\n");
         }
 
-        public void WriteOtherParameterFunction(StringBuilder builder, string name, int baseIndent)
+        public void WriteOtherParameterFunction(StringBuilder builder, string name, int baseIndent, int indentStep)
         {
             var indent = new string(' ', baseIndent);
             builder.Append($"{indent}public void Reset{name}() => this.animator.ResetTrigger(this.{name}Id);\n");
